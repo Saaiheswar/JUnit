0bf8529ee7143f1c9ab99632617444285bc0fa26
Saaiheswar <saaiheswar@gmail.com>
2020-03-25 21:47:37 +0530

    commit

---
+++ src/main/java/j_unit/App.java
@@ -0,0 +1,13 @@
+package j_unit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/j_unit/AppTest.java
@@ -0,0 +1,48 @@
+package j_unit;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class AppTest
+{
+    @Test
+    public void FirstLetterA()
+    {
+        RemovingA removingA = new RemovingA();
+        assertEquals("BCD",removingA.check("ABCD"));
+    }
+    @Test
+    public void BothLetterA(){
+        RemovingA removingA = new RemovingA();
+        assertEquals("BC",removingA.check("AABC"));
+    }
+    @Test
+    public void SECOndLetterA(){
+        RemovingA removingA = new RemovingA();
+        assertEquals("BCD",removingA.check("BACD"));
+    }
+    @Test
+    public void NoLetterA(){
+        RemovingA removingA = new RemovingA();
+        assertEquals("BBAA",removingA.check("BBAA"));
+    }
+    @Test
+    public void SingleLetterA(){
+        RemovingA removingA = new RemovingA();
+        assertEquals(" ",removingA.check("A"));
+    }
+    @Test
+    public void SomEOtherSingleLetter(){
+        RemovingA removingA = new RemovingA();
+        assertEquals("B",removingA.check("B"));
+    }
+    @Test
+    public void Empty(){
+        RemovingA removingA = new RemovingA();
+        assertEquals(" ",removingA.check(""));
+    }
+}
